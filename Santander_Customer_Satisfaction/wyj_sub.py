c5becdc5aa5c2aebebd57c3811fb411429e2191b
wyj2046 <wyj2046@gmail.com>
2016-03-22 15:50:05 +0800

    数据预处理, 去除重复列和常数列

--- Santander_Customer_Satisfaction/wyj_sub.py
+++ Santander_Customer_Satisfaction/wyj_sub.py
@@ -2,6 +2,23 @@
 import pandas as pd
 import xgboost as xgb
 from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+
+def get_remove_col(train):
+    remove = []
+    # 去除常数列
+    for c in train.columns:
+        if train[c].std() == 0:
+            remove.append(c)
+    # 去除重复的列
+    columns = train.columns
+    for i in range(len(columns) - 1):
+        v = train[columns[i]].values
+        for j in range(i + 1, len(columns)):
+            if np.array_equal(v, train[columns[j]].values):
+                remove.append(columns[j])
+    return remove
 
 
 def get_pred_y1(train_X, train_y, test_X):
@@ -21,6 +38,7 @@ def get_pred_y1(train_X, train_y, test_X):
     watchlist = [(xg_train_X, 'train')]
     # num_round = cv_result.shape[0]
     num_round = 57
+    print 'num_round', num_round
     bst = xgb.train(param, xg_train_X, num_round, watchlist)
 
     pred_y = bst.predict(xg_test_X)
@@ -37,16 +55,21 @@ def get_pred_y2(train_X, train_y, test_X):
 if __name__ == '__main__':
     train = pd.read_csv('train.csv')
     test = pd.read_csv('test.csv')
+
+    remove = get_remove_col(train)
+    train.drop(remove, axis=1, inplace=True)
+    test.drop(remove, axis=1, inplace=True)
+
     train_X = train.drop(['ID', 'TARGET'], axis=1)
     train_y = train['TARGET']
     test_X = test.drop(['ID'], axis=1)
 
     pred_y1 = get_pred_y1(train_X, train_y, test_X)
-    pred_y2 = get_pred_y2(train_X, train_y, test_X)
+    # pred_y2 = get_pred_y2(train_X, train_y, test_X)
 
-    pred_y = (pred_y1 * 4 + pred_y2 * 1) / 5
+    # pred_y = (pred_y1 * 4 + pred_y2 * 1) / 5
+    pred_y = pred_y1
 
-    submission = pd.DataFrame(data=pred_y, columns=['TARGET'])
-    submission = submission.join(test['ID'])
+    submission = pd.DataFrame({"ID": test['ID'], 'TARGET': pred_y})
     columns = ['ID', 'TARGET']
     submission.to_csv('result.csv', index=False, columns=columns)
